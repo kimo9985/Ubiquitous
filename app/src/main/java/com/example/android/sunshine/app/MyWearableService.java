b7c279a3db0f1bae9e8a7d2f4fa9f2d93b1de6f6
Kimo <jimtrinh9985@gmail.com>
2016-04-04 21:42:59 -0700

    feat:  watch face xml design

---
+++ app/src/main/java/com/example/android/sunshine/app/MyWearableService.java
@@ -0,0 +1,24 @@
+package com.example.android.sunshine.app;
+
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class MyWearableService extends WearableListenerService {
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        for (DataEvent dataEvent : dataEvents) {
+            if (dataEvent.getType() == DataEvent.TYPE_CHANGED) {
+                DataMap dataMap = DataMapItem.fromDataItem(dataEvent.getDataItem()).getDataMap();
+                String path = dataEvent.getDataItem().getUri().getPath();
+                if (path.equals("/weather-temp")) {
+                    int temp = dataMap.getInt("temp");
+                    long timestamp = dataMap.getLong("timestamp");
+                }
+            }
+        }
+    }
+}
